d0d745dc3c83462d2169694ddf364480a6eccff1
Anka Markovic-Borak <cubicabubica@gmail.com>
2017-12-07 14:26:18 +0100

    Implementacija lead-slidera

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+    
+    // Kada skrolujem 150px, da se smanji padding headera
+    
     $(window).scroll(function(){
         var scroll = $(window).scrollTop();
         if(scroll > 150){
@@ -8,12 +11,26 @@ $(document).ready(function(){
         }
     });
     
+    // kada kliknem na link u nav, da polako spusti na taj deo
     
     $('.navbar-nav a').click(function(){
         $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 2000);
     });
+    
+    
+    // prvi bxslider
+    
+    if($('.bxslider').length > 0){
+        $('.lead-slider').bxSlider();
+    }
+    
+    
+    
+    
+    
+    
     
     
     
