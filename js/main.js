75aa5f7a2504d172a4f376fdf66a38e01674be9c
Anka Markovic-Borak <cubicabubica@gmail.com>
2017-12-07 15:26:58 +0100

    Sredjivanje team i portfolio sekcije

--- js/main.js
+++ js/main.js
@@ -1,48 +1,114 @@
-$(document).ready(function(){
-    
+$(document).ready(function () {
+
     // Kada skrolujem 150px, da se smanji padding headera
-    
-    $(window).scroll(function(){
+
+    $(window).scroll(function () {
         var scroll = $(window).scrollTop();
-        if(scroll > 150){
+        if (scroll > 150) {
             $('header').addClass('py-2');
-        }else{
-           $('header').removeClass('py-2'); 
+        } else {
+            $('header').removeClass('py-2');
         }
     });
-    
+
     // kada kliknem na link u nav, da polako spusti na taj deo
-    
-    $('.navbar-nav a').click(function(){
+
+    $('.navbar-nav a').click(function () {
         $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 2000);
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 2000);
     });
-    
-    
+
+
     // prvi bxslider
-    
-    if($('.bxslider').length > 0){
+
+    if ($('.bxslider').length > 0) {
         $('.lead-slider').bxSlider();
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 
-    
+
+    // animacija
+
+    function animation() {
+        var windowHeight = $(window).height();
+        var scroll = $(window).scrollTop();
+        $('.animation').each(function () {
+            var pozicija = $(this).offset().top;
+            var animacija = $(this).attr('data-animation');
+            if (pozicija < scroll + windowHeight - 100) {
+                $(this).addClass(animacija);
+            }/*else{
+             $(this).removeClass(animacija);
+             }*/
+        });
+    }
+
+    animation();
+    $(window).scroll(function () {
+        animation();
+    });
+
+
+    if ($('.owl-carousel').length > 0) {
+        $('.team-slider').owlCarousel({
+            loop: true,
+            margin: 10,
+            responsiveClass: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                480: {
+                    items: 2
+                },
+                768: {
+                    items: 3
+                },
+                992: {
+                    items: 4
+                }
+            }
+        });
+        
+        $('.portfolio-slider').owlCarousel({
+            loop: true,
+            margin: 10,
+            responsiveClass: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                480: {
+                    items: 2
+                },
+                768: {
+                    items: 3
+                },
+                992: {
+                    items: 4
+                },
+                1200: {
+                    items: 5
+                }
+            }
+        });
+        
+        
+        
+        
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
